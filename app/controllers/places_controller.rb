f80f933803e42dedb43ab75af883002568793137
Roberto de Farias <235542534+robertodefarias@users.noreply.github.com>
2026-03-23 15:15:45 -0300

    unificado

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -24,14 +24,8 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find(params[:id])
-<<<<<<< HEAD
     @reports = @place.reports.order(created_at: :desc)
     @report = @place.reports.build
-=======
-    @reports = @place.reports
-    @recent_reports = @place.reports.order(created_at: :desc).limit(4)
-    @report = Report.new
->>>>>>> origin/master
   end
 
   def create
